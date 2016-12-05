8637eed06719082e07bad0d8095ced4436f4202b
Saujanya  Nagpal <saujanyanagpal@Saujanyas-MacBook-Air.local>
2016-12-05 15:18:11 +0530

    some errors are fixed and test for name is passed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,21 +31,31 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('url is defined and shuld not be empty', function(){
-           for(var i = 0; i < allFeeds.length; i++)
+         it('url is defined and should not be empty', function(){
+           for(var i = 0; i < allFeeds.length; i++){
            expect(allFeeds[i].url).toBeDefined();
            expect(allFeeds[i].url.length).not.toBe(0);
+         }
          });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    });
+         it('name should be defined and not to be empty', function(){
+           for(var j = 0; j < allFeeds.length; j++){
+             var nam = allFeeds[j].name;
+             expect(nam).toBeDefined();
+             expect(nam.length).not.toBe(0);
+           }
+         });
+       });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+        describe('The menu', function(){
+          
+        });
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
